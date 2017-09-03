f298b716fc631291df642f0c639d8a11b0dd4e1b
胡玮文 <huww98@163.com>
2017-09-03 20:53:14 +0800

    Init commit

---
+++ ScutTimeTableExport/Lecture.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ScutTimeTableExport
+{
+    internal struct LessonTime
+    {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+
+    internal class Lecture
+    {
+        private static Regex timeRegex =
+            new Regex(@"周([一二三四五六日])第(\d{1,2}(?:,\d{1,2})*)节\{第(\d{1,2})-(\d{1,2})周(?:(\|单周)?|(\|双周)?)}",
+                RegexOptions.Compiled);
+
+        private static Dictionary<string, DayOfWeek> dayOfWeekName = new Dictionary<string, DayOfWeek>
+        {
+            {"日", DayOfWeek.Sunday},
+            {"一", DayOfWeek.Monday},
+            {"二", DayOfWeek.Tuesday},
+            {"三", DayOfWeek.Wednesday},
+            {"四", DayOfWeek.Thursday},
+            {"五", DayOfWeek.Friday},
+            {"六", DayOfWeek.Saturday},
+        };
+
+        private static Dictionary<int, LessonTime> lessonTime = new Dictionary<int, LessonTime>
+        {
+            {1,  new LessonTime{ StartTime = new TimeSpan(8, 50,0), EndTime = new TimeSpan(9, 35,0) } },
+            {2,  new LessonTime{ StartTime = new TimeSpan(9, 40,0), EndTime = new TimeSpan(10,25,0) } },
+            {3,  new LessonTime{ StartTime = new TimeSpan(10,40,0), EndTime = new TimeSpan(11,25,0) } },
+            {4,  new LessonTime{ StartTime = new TimeSpan(11,30,0), EndTime = new TimeSpan(12,15,0) } },
+            {5,  new LessonTime{ StartTime = new TimeSpan(14,00,0), EndTime = new TimeSpan(14,45,0) } },
+            {6,  new LessonTime{ StartTime = new TimeSpan(14,50,0), EndTime = new TimeSpan(15,35,0) } },
+            {7,  new LessonTime{ StartTime = new TimeSpan(15,45,0), EndTime = new TimeSpan(16,30,0) } },
+            {8,  new LessonTime{ StartTime = new TimeSpan(16,35,0), EndTime = new TimeSpan(17,20,0) } },
+            {9,  new LessonTime{ StartTime = new TimeSpan(19,00,0), EndTime = new TimeSpan(19,45,0) } },
+            {10, new LessonTime{ StartTime = new TimeSpan(19,50,0), EndTime = new TimeSpan(20,35,0) } },
+            {11, new LessonTime{ StartTime = new TimeSpan(20,40,0), EndTime = new TimeSpan(21,25,0) } },
+        };
+
+        public string Name { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public int[] IndexOfLessons { get; set; }
+        public int StartWeek { get; set; }
+        public int WeekInterval { get; set; }
+        public int RepeatTime { get; set; }
+        public string Teacher { get; set; }
+        public string Address { get; set; }
+
+        public TimeSpan StartTime => lessonTime[IndexOfLessons[0]].StartTime;
+        public TimeSpan EndTime => lessonTime[IndexOfLessons[IndexOfLessons.Length - 1]].EndTime;
+
+        public Lecture(string str)
+        {
+            var lines = str.Split('\n');
+            Name = lines[0];
+
+            var match = timeRegex.Match(lines[1]);
+            DayOfWeek = dayOfWeekName[match.Groups[1].Value];
+            IndexOfLessons = match.Groups[2].Value.Split(',').Select(s => int.Parse(s)).ToArray();
+            StartWeek = int.Parse(match.Groups[3].Value);
+            var endWeek = int.Parse(match.Groups[4].Value);
+
+            bool oddWeek = match.Groups[5].Success;
+            bool evenWeek = match.Groups[6].Success;
+            if (oddWeek)
+            {
+                WeekInterval = 2;
+                if (StartWeek % 2 == 0)
+                {
+                    StartWeek++;
+                }
+                if (endWeek % 2 == 0)
+                {
+                    endWeek--;
+                }
+            }
+            if (evenWeek)
+            {
+                WeekInterval = 2;
+                if (StartWeek % 2 == 1)
+                {
+                    StartWeek++;
+                }
+                if (endWeek % 2 == 1)
+                {
+                    endWeek--;
+                }
+            }
+            else
+            {
+                WeekInterval = 1;
+            }
+            RepeatTime = (endWeek - StartWeek) / WeekInterval + 1;
+
+            Teacher = lines[2];
+            Address = lines[3];
+        }
+    }
+}
--- ScutTimeTableExport/Program.cs
+++ ScutTimeTableExport/Program.cs
@@ -1,12 +1,90 @@
-﻿using System;
+﻿using AngleSharp;
+using AngleSharp.Dom.Html;
+using Ical.Net;
+using Ical.Net.DataTypes;
+using Ical.Net.Interfaces.DataTypes;
+using Ical.Net.Serialization.iCalendar.Serializers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ScutTimeTableExport
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("请前往http://jw2005.scuteo.com/进行登录（最好启用隐私模式），将登录后的网址粘贴于本程序中");
+            Console.WriteLine("请注意，上面的网址不是一般登录教务系统时使用的网址");
+            Console.WriteLine("警告：在平时使用教务系统时，请勿将此网址。登录后的网址包含有您的个人敏感信息，请勿泄漏");
+
+            var address = Console.ReadLine();
+
+            Regex regex = new Regex(@"http://xsweb\.scuteo\.com/\((.*?)\)/xs_main\.aspx\?xh=(\d{12})", RegexOptions.IgnoreCase);
+
+            var match = regex.Match(address);
+            if (!match.Success)
+            {
+                Console.WriteLine("网址未能识别");
+                return;
+            }
+
+            var token = match.Groups[1].Value;
+            var studnetNum = match.Groups[2].Value;
+
+            var queryAddress = $"http://xsweb.scuteo.com/({token})/xskbcx.aspx?xh={studnetNum}&gnmkdm=N121603";
+
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+
+            var main = context.OpenAsync(address).Result;
+            var lectureTable = context.OpenAsync(queryAddress).Result;
+
+            var table = lectureTable.GetElementById("Table1") as IHtmlTableElement;
+            var lectureTexts = table.Rows
+                .SelectMany(r => r.Cells.Where(c => c.TextContent.Length > 5))
+                .Select(c => c.InnerHtml.Replace("<br>", "\n"));
+
+            var lectures = new List<Lecture>();
+            foreach (var lt in lectureTexts)
+            {
+                lectures.Add(new Lecture(lt));
+            }
+
+            var calendar = new Calendar();
+            DateTime SchoolBeginTime = new DateTime(2017, 9, 3);
+
+            foreach (var l in lectures)
+            {
+                var date = SchoolBeginTime
+                    + TimeSpan.FromDays(7 * (l.StartWeek - 1))
+                    + TimeSpan.FromDays((int)l.DayOfWeek);
+                var e = new Event
+                {
+                    DtStart = new CalDateTime(date + l.StartTime),
+                    DtEnd = new CalDateTime(date + l.EndTime),
+                    Summary = l.Name,
+                    Location = l.Address,
+                    RecurrenceRules = new List<IRecurrencePattern>
+                    {
+                        new RecurrencePattern(FrequencyType.Weekly, l.WeekInterval){Count = l.RepeatTime}
+                    },
+                    Description = $"老师:{l.Teacher}",
+                };
+                calendar.Events.Add(e);
+            }
+
+            var serializer = new CalendarSerializer(calendar);
+
+            const string fileName = "课表.ics";
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                serializer.Serialize(calendar, stream, Encoding.UTF8);
+            }
+            Console.WriteLine($"处理完成，发现课程{lectures.Count}门，已导出到“{fileName}”");
         }
     }
 }
