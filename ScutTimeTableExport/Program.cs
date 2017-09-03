87480925847c492db844b8c84daa1fe2f7bb7dc1
胡玮文 <huww98@163.com>
2017-09-03 20:48:24 +0800

    添加项目文件。

---
+++ ScutTimeTableExport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScutTimeTableExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
